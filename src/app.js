3f11cbfa89fd1e98923131db02d2cafeea2906e6
Nick Costello <nick@nickcostello.dev>
2021-07-26 21:22:04 -0400

    NodeJS boilerplate app - v1.0.0

---
+++ src/app.js
@@ -0,0 +1,11 @@
+/**
+ * Returns the sum of a and b.
+ * @param {number} a - the first number to add.
+ * @param {number} b - the second number to add.
+ * @returns {number} the sum of a and b.
+ */
+const sum = (a, b) => {
+	return a + b
+}
+
+module.exports = sum
